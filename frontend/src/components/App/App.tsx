bca9500850076afc0453674c3ae3f7dacda3eb07
Matt Davies <matthew.davies@bjss.com>
2020-11-22 17:10:35 +0000

    todo with hooks and theme ui for styles

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import ToDo from '../Todo/Todo';
+import { ThemeProvider } from 'theme-ui';
+import theme from '../../theme/index';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <ToDo />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <ToDo />
+      </div>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/components/Todo/Todo.tsx
+++ frontend/src/components/Todo/Todo.tsx
@@ -1,42 +1,64 @@
-import React, { useState } from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { useState } from 'react';
+import { jsx } from 'theme-ui';
+import moment from 'moment';
 
 const ToDo = () => {
   const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState([{}]);
-
+  const [todos, setTodos] = useState([]);
   const handleNewTodoChange = (e: any) => {
     e.preventDefault();
     setNewTodo(e.target.value);
   };
   const handleNewTodo = (e: any) => {
     e.preventDefault();
-    if (newTodo === '') {
-      return;
-    }
-    setTodos([...todos, { id: Date.now(), text: newTodo }]);
-    setNewTodo('');
+    if (newTodo === '') return;
+    const newTodoArray: any = [...todos, { id: moment().format('MMMM Do YYYY, h:mm:ss a'), text: newTodo }];
+    setTodos(newTodoArray);
     e.target.reset();
+    setNewTodo('');
   };
   const removeTodo = (id: any) => {
     setTodos(todos.filter((todo: any) => todo.id != id));
   };
   return (
-    <>
-      <h1>Todo</h1>
+    <div sx={{ width: 'todoWidth', margin: 'auto', textAlign: 'left' }}>
       <form onSubmit={handleNewTodo}>
-        <input placeholder='Enter todo...' onChange={handleNewTodoChange}></input>
+        <div
+          sx={{
+            margin: 'm',
+          }}
+        >
+          <h1 sx={{ fontFamily: 'title', fontSize: 'xxxxl', color: 'text', textAlign: 'center', margin: 'm' }}>Todo</h1>
+        </div>
+        <input placeholder='Enter todo...' onChange={handleNewTodoChange} sx={{ display: 'block', margin: 'auto' }}></input>
         <ul>
           {todos.map((todo: any) => (
-            <li key={todo.id}>
-              {todo.text}
-              <a href='#' onClick={() => removeTodo(todo.id)}>
-                X
+            <li
+              key={todo.id}
+              sx={{
+                listStyle: 'none',
+                padding: 'm',
+                margin: 's',
+                backgroundColor: 'panel',
+                borderRadius: 'l',
+              }}
+            >
+              <a
+                href='#'
+                onClick={() => removeTodo(todo.id)}
+                sx={{ float: 'right', textDecoration: 'none', color: 'text', fontSize: 'xl' }}
+              >
+                x
               </a>
+              <h3 sx={{ overflowWrap: 'break-word', wordWrap: 'break-word' }}>{todo.text}</h3>
+              <p sx={{ display: 'inline-block' }}>Created: {todo.id}</p>
             </li>
           ))}
         </ul>
       </form>
-    </>
+    </div>
   );
 };
 
